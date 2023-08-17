6a86eb537b763498163da90d94fd983d4788eca3
victoriafaj <84591393+victoriafaj@users.noreply.github.com>
2023-08-17 09:46:28 -0300

    Add files via upload

---
+++ archivo.js
@@ -0,0 +1,18 @@
+function cambiarColor() {
+    let tit = document.getElementById('titulo')
+    tit.style.color = '#ff0000'
+}
+function Fuentepequeña() {
+    let tit = document.getElementById('titulo')
+    tit.style.fontSize = '10px'
+}
+
+function Fuentemediana() {
+    let tit = document.getElementById('titulo')
+    tit.style.fontSize = '13px'
+}
+
+function Fuentegrande() {
+    let tit = document.getElementById('titulo')
+    tit.style.fontSize = '20px'
+}
